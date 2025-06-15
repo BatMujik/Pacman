2c99ec6b0c47658d131e5aa4a72940f554770b38
BatMujik <79758372+BatMujik@users.noreply.github.com>
2025-06-15 23:33:22 +0400

    Add files via upload

---
+++ build/CMakeFiles/Pacman.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/artyom/Desktop/thomsons/Games/Pacman/Source/ConvertSketch.cpp" "CMakeFiles/Pacman.dir/Source/ConvertSketch.cpp.o" "gcc" "CMakeFiles/Pacman.dir/Source/ConvertSketch.cpp.o.d"
+  "/home/artyom/Desktop/thomsons/Games/Pacman/Source/DrawMap.cpp" "CMakeFiles/Pacman.dir/Source/DrawMap.cpp.o" "gcc" "CMakeFiles/Pacman.dir/Source/DrawMap.cpp.o.d"
+  "/home/artyom/Desktop/thomsons/Games/Pacman/Source/DrawText.cpp" "CMakeFiles/Pacman.dir/Source/DrawText.cpp.o" "gcc" "CMakeFiles/Pacman.dir/Source/DrawText.cpp.o.d"
+  "/home/artyom/Desktop/thomsons/Games/Pacman/Source/Ghost.cpp" "CMakeFiles/Pacman.dir/Source/Ghost.cpp.o" "gcc" "CMakeFiles/Pacman.dir/Source/Ghost.cpp.o.d"
+  "/home/artyom/Desktop/thomsons/Games/Pacman/Source/GhostManager.cpp" "CMakeFiles/Pacman.dir/Source/GhostManager.cpp.o" "gcc" "CMakeFiles/Pacman.dir/Source/GhostManager.cpp.o.d"
+  "/home/artyom/Desktop/thomsons/Games/Pacman/Source/Main.cpp" "CMakeFiles/Pacman.dir/Source/Main.cpp.o" "gcc" "CMakeFiles/Pacman.dir/Source/Main.cpp.o.d"
+  "/home/artyom/Desktop/thomsons/Games/Pacman/Source/MapCollision.cpp" "CMakeFiles/Pacman.dir/Source/MapCollision.cpp.o" "gcc" "CMakeFiles/Pacman.dir/Source/MapCollision.cpp.o.d"
+  "/home/artyom/Desktop/thomsons/Games/Pacman/Source/Pacman.cpp" "CMakeFiles/Pacman.dir/Source/Pacman.cpp.o" "gcc" "CMakeFiles/Pacman.dir/Source/Pacman.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Pacman.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pacman.dir/Source/ConvertSketch.cpp.o"
+  "CMakeFiles/Pacman.dir/Source/ConvertSketch.cpp.o.d"
+  "CMakeFiles/Pacman.dir/Source/DrawMap.cpp.o"
+  "CMakeFiles/Pacman.dir/Source/DrawMap.cpp.o.d"
+  "CMakeFiles/Pacman.dir/Source/DrawText.cpp.o"
+  "CMakeFiles/Pacman.dir/Source/DrawText.cpp.o.d"
+  "CMakeFiles/Pacman.dir/Source/Ghost.cpp.o"
+  "CMakeFiles/Pacman.dir/Source/Ghost.cpp.o.d"
+  "CMakeFiles/Pacman.dir/Source/GhostManager.cpp.o"
+  "CMakeFiles/Pacman.dir/Source/GhostManager.cpp.o.d"
+  "CMakeFiles/Pacman.dir/Source/Main.cpp.o"
+  "CMakeFiles/Pacman.dir/Source/Main.cpp.o.d"
+  "CMakeFiles/Pacman.dir/Source/MapCollision.cpp.o"
+  "CMakeFiles/Pacman.dir/Source/MapCollision.cpp.o.d"
+  "CMakeFiles/Pacman.dir/Source/Pacman.cpp.o"
+  "CMakeFiles/Pacman.dir/Source/Pacman.cpp.o.d"
+  "Pacman"
+  "Pacman.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pacman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
